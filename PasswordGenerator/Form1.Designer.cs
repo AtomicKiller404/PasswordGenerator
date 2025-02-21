38cd42eee9a1ed177627fd023cd6963067d1546c
Miha Plibersek <miha.plib@gmail.com>
2025-02-21 19:39:40 +0100

    Add project files.

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.uppercase_checkBox = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lowercase_checkBox = new System.Windows.Forms.CheckBox();
+            this.num_checkBox = new System.Windows.Forms.CheckBox();
+            this.symbols_checkBox = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(279, 230);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.passwordBox.Location = new System.Drawing.Point(3, 6);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.ReadOnly = true;
+            this.passwordBox.Size = new System.Drawing.Size(150, 20);
+            this.passwordBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password length ";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.numericUpDown1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(61, 47);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(156, 26);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(97, 3);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // uppercase_checkBox
+            // 
+            this.uppercase_checkBox.AutoSize = true;
+            this.uppercase_checkBox.Location = new System.Drawing.Point(3, 3);
+            this.uppercase_checkBox.Name = "uppercase_checkBox";
+            this.uppercase_checkBox.Size = new System.Drawing.Size(145, 17);
+            this.uppercase_checkBox.TabIndex = 4;
+            this.uppercase_checkBox.Text = "Include uppercase letters";
+            this.uppercase_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.flowLayoutPanel2.Controls.Add(this.uppercase_checkBox);
+            this.flowLayoutPanel2.Controls.Add(this.lowercase_checkBox);
+            this.flowLayoutPanel2.Controls.Add(this.num_checkBox);
+            this.flowLayoutPanel2.Controls.Add(this.symbols_checkBox);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(61, 79);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(156, 100);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // lowercase_checkBox
+            // 
+            this.lowercase_checkBox.AutoSize = true;
+            this.lowercase_checkBox.Location = new System.Drawing.Point(3, 26);
+            this.lowercase_checkBox.Name = "lowercase_checkBox";
+            this.lowercase_checkBox.Size = new System.Drawing.Size(143, 17);
+            this.lowercase_checkBox.TabIndex = 5;
+            this.lowercase_checkBox.Text = "Include lowercase letters";
+            this.lowercase_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // num_checkBox
+            // 
+            this.num_checkBox.AutoSize = true;
+            this.num_checkBox.Location = new System.Drawing.Point(3, 49);
+            this.num_checkBox.Name = "num_checkBox";
+            this.num_checkBox.Size = new System.Drawing.Size(104, 17);
+            this.num_checkBox.TabIndex = 6;
+            this.num_checkBox.Text = "Include numbers";
+            this.num_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // symbols_checkBox
+            // 
+            this.symbols_checkBox.AutoSize = true;
+            this.symbols_checkBox.Location = new System.Drawing.Point(3, 72);
+            this.symbols_checkBox.Name = "symbols_checkBox";
+            this.symbols_checkBox.Size = new System.Drawing.Size(101, 17);
+            this.symbols_checkBox.TabIndex = 7;
+            this.symbols_checkBox.Text = "Include symbols";
+            this.symbols_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.passwordBox);
+            this.flowLayoutPanel3.Controls.Add(this.button1);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(41, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(197, 38);
+            this.flowLayoutPanel3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(159, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "📋";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(41, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Generate password";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 230);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.CheckBox uppercase_checkBox;
+        private System.Windows.Forms.CheckBox lowercase_checkBox;
+        private System.Windows.Forms.CheckBox num_checkBox;
+        private System.Windows.Forms.CheckBox symbols_checkBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        private int length = 8;
+        private string upper_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string lower_chars = "abcdefghijklmnopqrstuvwxyz";
+        private string num_chars = "0123456789";
+        private string symbols_chars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = length;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Generate Password
+        {
+            if (length == 0) return;
+            string chars = "";
+
+            if (uppercase_checkBox.Checked) chars += upper_chars;
+            if (lowercase_checkBox.Checked) chars += lower_chars;
+            if (num_checkBox.Checked) chars += num_chars;
+            if (symbols_checkBox.Checked) chars += symbols_chars;
+
+            if (chars == "") return;
+
+            passwordBox.Text = generatePassword(chars, length);
+
+            chars = "";
+        }
+
+        private string generatePassword(string chars, int length)
+        {
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] data = new byte[length];
+                rng.GetBytes(data);
+
+                StringBuilder result = new StringBuilder(length);
+                foreach (byte b in data)
+                {
+                    result.Append(chars[b % chars.Length]);
+                }
+
+                return result.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(passwordBox.Text))
+            {
+                Clipboard.SetText(passwordBox.Text);
+                MessageBox.Show("Password copied to clipboard!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No password generated yet!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            length = (int)numericUpDown1.Value;
+        }
+    }
+}
